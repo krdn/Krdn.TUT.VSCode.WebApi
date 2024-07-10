d992fa61e565d1e35e6293784b37963b8cd8fa2b
Gon Kim <krdn.net@gmail.com>
2024-07-10 22:29:23 +0900

    chore: Delete stock data endpoint in StockController

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -74,6 +74,22 @@ namespace api.Controllers
             return Ok(stockModel.ToStockDto());
         }
 
+        [HttpDelete("{id}")]
+        // [Route("{id}")]
+        public IActionResult DeleteStock([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(stock => stock.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent(); // 204
+        }
+
 
     }
 }
