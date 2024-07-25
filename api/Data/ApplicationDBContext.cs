50284dd3d6124c723833bfedb053bf91f44622b6
Gon Kim <krdn.net@gmail.com>
2024-07-26 08:50:36 +0900

    chore: Seed database with default roles

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -29,6 +29,12 @@ public class ApplicationDBContext : IdentityDbContext<AppUser>
             new IdentityRole { Name = "User", NormalizedName = "USER" }
         };
 
+        // Seed 데이터 추가
+        // https://docs.microsoft.com/ko-kr/ef/core/modeling/data-seeding
+        // https://docs.microsoft.com/ko-kr/ef/core/modeling/data-seeding#using-an-entity-type-to-configure-data-seeding
+        // dotnet ef migrations add SeedRole
+        // dotnet ef database update
+        // Migrations폴더에 자동으로 SeedRole.cs 파일이 생성되고, 데이터베이스에 데이터가 추가됩니다.
         builder.Entity<IdentityRole>().HasData(roles);
     }
 
